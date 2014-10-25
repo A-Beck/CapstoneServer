18d3ad6e01377ea9f9490549fb791cb1ae546c03
A-Beck <adb2jb@virginia.edu>
2014-10-25 13:30:40 -0400

    Adding Functionality, like updating device temperatures

--- CapstoneServer.py
+++ CapstoneServer.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
 import time
+import random
 
 # setup environment stuff
 app = Flask(__name__)
@@ -9,14 +10,16 @@ db = SQLAlchemy(app)
 
 # TODO: Create a table that holds user credentials
 # TODO: create a table that relates users to devices
+# TODO: Error Checking for all user generated values
+# TODO: Error Checking for DB accesses
 
 
 # This is a class that relates devices to actions they have to perform
 class DeviceAction(db.Model):
     __tablename__ = 'device_action'
     id = db.Column(db.Integer, primary_key=True)
-    device = db.Column(db.Text, unique=False)
-    action = db.Column(db.Text, unique=False)
+    device = db.Column(db.String(20), unique=False)
+    action = db.Column(db.String(50), unique=False)
     time = db.Column(db.Integer)
     complete = db.Column(db.Boolean, unique=False)
 
@@ -25,6 +28,14 @@ class DeviceAction(db.Model):
         self.action = action
         self.complete = False
         self.time = time.time()
+
+
+class DeviceStatus(db.Model):
+    __tablename__ = 'device_status'
+    device = db.Column(db.String(20), primary_key=True)
+    temp = db.Column(db.Float, unique=False)
+    humidity = db.Column(db.Float, unique=False)
+    time = db.Column(db.Integer)
 
 
 #the homepage for our app
@@ -53,10 +64,16 @@ def actions():
 
 
 # endpoint for a device - the url it hits to get it's next command
-@app.route('/getcommand')
-def getcommand():
+@app.route('/getcommand/<device>')
+def getcommand(device):
     # TODO: add logic that will send responses based on info in DB
-    return 1
+    formated_string = ''
+    # This line gets all uncompleted actions for the specified device
+    commands = DeviceAction.query.filter_by(device=device, complete=False)
+    for command in commands:
+        formated_string += ' ' + str(command.action) + ' ' + str(command.id)
+    number = random.randint(0, 1)
+    return str(number)
 
 
 #adding a command to a device's command queue
@@ -73,19 +90,31 @@ def addcommand():
     
     
 # TODO: add route that allows mC to mark task as done
-@app.route('/completetask', methods=['POST'])
-def completetask():
-    pass
+@app.route('/completetask/<taskid>')
+def completetask(taskid):
+    try:
+        device_action = DeviceAction.query.get(taskid)
+        device_action.complete = True
+        db.session.commit()
+        return '1'
+    except Exception as e:
+        print e
+        return '0'
 
 
 # TODO: add route that will allow mC to post temp data
-@app.route('/sendtemp', methods=['POST'])
-def sendtemp():
-    pass
+@app.route('/sendtemp/device=<device>&temp=<temp>')
+def sendtemp(device, temp):
+    try:
+        device = DeviceStatus.query.get(device)
+        device.temp = temp
+        device.time = time.time()
+        db.session.commit()
+        return '1'
+    except Exception as e:
+        print e
+        return '0'
     
-
-
-
 
 app.debug = True
 
